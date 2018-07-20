dc583535baf6c02e44c2cdbd3a306a9e7a7f6b06
tpalayda <taras.palayda@gmail.com>
2018-07-20 18:37:47 +0200

    t7

--- t7.cpp
+++ t7.cpp
@@ -5,13 +5,18 @@
 int reverse(int x)
 {
     int reversed_num = x % 10;
+    int max_val = std::numeric_limits<int>::max()/10;
+    int min_val = std::numeric_limits<int>::min()/10;
     while(x/=10)
+    {
+        if(reversed_num > max_val || reversed_num < min_val)
+            return 0;
         reversed_num = reversed_num*10 + (x % 10); //may overflow
+    }
     return reversed_num;
 }
 
 int main()
 {
-    std::cout << reverse(321) << std::endl;
- //   std::cout << std::numeric_limits<int>::max() << std::endl; 
+    std::cout << reverse(1056389759) << std::endl;
 }
