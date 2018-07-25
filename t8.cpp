c92fad07b5ca5814c3900a080052f6ebfc6d016a
tpalayda <taras.palayda@gmail.com>
2018-07-25 12:01:34 +0200

    1015/1079 test cases passed

--- t8.cpp
+++ t8.cpp
@@ -25,19 +25,28 @@ int myAtoi(const std::string& str)
         }
         if(isNumeric(*it))
             if(!isNegative && result > max_val)
-                return max_val*10+7;
+                return max_val*10;
             else if(isNegative && result*-1 < min_val)
-                return min_val*10-8;
+                return min_val*10;
             else
+            {
                 result = result*10 + *it - 48;
+                std::cout << result << std::endl;
+                if(*(it+1) == ' ')
+                    return result;
+            }
         else if(*it == '.')
             return result;
+        else if(*it == ' ' || *it == '+')
+            continue;
+        else
+            break;
     }
     result = isNegative ? result*-1 : result;
     return result;
 }
 int main()
 {
-    std::string a = "1";
+    std::string a = "2147483648";
     std::cout << myAtoi(a) << std::endl;
 }
