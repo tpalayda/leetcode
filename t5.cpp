3ad47ee4830e285e825a36e10b7c308118b6ff9c
tpalayda <taras.palayda@gmail.com>
2018-07-27 14:33:17 +0200

    beginning of t5

---
+++ t5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+std::string longestPalindrome(const std::string& s)
+{
+    return "";
+}
+
+int main()
+{
+    std::cout << longestPalindrome("babad") << std::endl;
+}
