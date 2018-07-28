0581720a70b57d60d49a15c909ca34bff338b756
tpalayda <taras.palayda@gmail.com>
2018-07-28 11:48:22 +0200

    just saving idea(code doesn't work)

--- t5.cpp
+++ t5.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_set>
 
+//we may assume that the maximum length of s <= 1000
+bool isPalindrome(const std::string& s)
+{
+    return s == std::string(s.rbegin(),s.rend());
+}
 std::string longestPalindrome(const std::string& s)
 {
-    return "";
+    if(isPalindrome(s))
+        return s;
+    std::unordered_set<char> set;
+    std::string result;
+    std::string temp_result;
+    for(auto it = s.begin(); it != s.end(); ++it)
+    {
+        if(set.count(*it) && (*it != *(it+1) || isPalindrome(temp_result+*(it+1))))
+        {
+            temp_result += *it; 
+            if(isPalindrome(temp_result) && temp_result.size() > result.size())
+                result = std::move(temp_result);
+            set.clear();
+        }
+        set.emplace(*it);
+        temp_result += *it;
+    }
+    return result;
 }
 
 int main()
 {
-    std::cout << longestPalindrome("babad") << std::endl;
+    std::cout << longestPalindrome("ababad") << std::endl;
+    //std::cout << isPalindrome("aaaaa") << std::endl;
 }
