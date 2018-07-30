ad26099a6b4b1b3cc0bb2cec1cb3686faebc3e8e
tpalayda <taras.palayda@gmail.com>
2018-07-30 20:59:05 +0200

    t12 idea

--- t12.cpp
+++ t12.cpp
@@ -8,11 +8,29 @@ const char RomanSymbol(const unsigned& num)
         case 2:
         case 3:
         case 4:
-        case 5:
         case 6:
         case 7:
         case 8:
         case 9:
+            return 'I';
+            break;
+        case 5:
+            return 'V';
+            break;
+        case 10:
+            return 'X';
+            break;
+        case 50:
+            return 'L';
+            break;
+        case 100:
+            return 'C';
+            break;
+        case 500:
+            return 'D';
+            break;
+        case 1000:
+            return 'M';
             break;
         default:
             break;
@@ -24,9 +42,12 @@ std::string intToRoman(const unsigned& num)
     std::string result;
     while(exp*10 <= num)
         exp *= 10;
+    unsigned numb = 0;
+    std::cout << "exp:" << exp << std::endl;
     for(unsigned i = exp; i; i /= 10)
     {
-        std::cout << num/i%10 << std::endl;
+        numb = (num/i%10)*i;
+        result += RomanSymbol(numb);
     }
     return result;
 }
