04413e244e204fd51dea389e3f0f7ee69dc0878e
tpalayda <taras.palayda@gmail.com>
2018-08-01 16:09:51 +0200

    t20

--- t20.cpp
+++ t20.cpp
@@ -12,22 +12,20 @@ bool isValid(const std::string& s)
         return true;
     if(n & 1)
         return false;
-    
-    std::vector<char> open_brackets;
-    std::vector<char> closed_brackets;
-
-    open_brackets.reserve(n/2);
-    closed_brackets.reserve(n/2);
-
+    std::vector<char> v;
+    v.reserve(n/2);
     for(const char& ch : s)
-        if(ch == '(' || ch == '[' || ch == '{')
-            open_brackets.emplace_back(ch);
-        else
-            closed_brackets.emplace_back(ch);
-    for(unsigned i = 0; i < open_brackets.size(); ++i)
-        if(!checkBrackets(open_brackets[open_brackets.size()-1-i],closed_brackets[i]))
+        if(ch == '(')
+            v.emplace_back(')');
+        else if(ch == '[')
+            v.emplace_back(']');
+        else if(ch == '{')
+            v.emplace_back('}');
+        else if(v.empty() || v.back() != ch)
             return false;
-    return true;
+        else
+            v.pop_back();
+    return v.empty();
 }
 int main()
 {
