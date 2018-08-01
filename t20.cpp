d10fcd0503d4365bccafc661ca7e97ddb4db31bf
tpalayda <taras.palayda@gmail.com>
2018-08-01 15:39:32 +0200

    t20 58/76 test cases passed

--- t20.cpp
+++ t20.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
+#include <vector>
 
+constexpr bool checkBrackets(const char& i, const char& j)
+{
+    return (i == '(' && j == ')') || (i == '[' && j == ']') || (i == '{' && j == '}') ? true : false;
+}
 bool isValid(const std::string& s)
 {
+    const size_t n = s.size();
     if(s.empty())
         return true;
-    if(s.size() & 1)
+    if(n & 1)
         return false;
-    bool isValid = false;
-    for(unsigned i = 0, j = s.size()-1; i < s.size()/2; ++i, --j)
-    {
-        std::cout << s[i] << "," << s[j] << std::endl;
-        isValid = (s[i] == '(' && s[j] == ')') || (s[i] == '[' && s[j] == ']') || (s[i] == '{' && s[j] == '}') ? true : false;
-    }
-    return isValid;
+    
+    std::vector<char> open_brackets;
+    std::vector<char> closed_brackets;
+
+    open_brackets.reserve(n/2);
+    closed_brackets.reserve(n/2);
+
+    for(const char& ch : s)
+        if(ch == '(' || ch == '[' || ch == '{')
+            open_brackets.emplace_back(ch);
+        else
+            closed_brackets.emplace_back(ch);
+    for(unsigned i = 0; i < open_brackets.size(); ++i)
+        if(!checkBrackets(open_brackets[open_brackets.size()-1-i],closed_brackets[i]))
+            return false;
+    return true;
 }
 int main()
 {
-    std::cout << isValid("(])") << "\n";
+    std::cout << isValid("()[]{}") << "\n";
 }
