961db3a9205df549fb79813e63bfdf6d8fb8f5fa
tpalayda <taras.palayda@gmail.com>
2018-08-01 17:23:44 +0200

    t26

--- t26.cpp
+++ t26.cpp
@@ -5,17 +5,15 @@
 
 unsigned removeDuplicates(std::vector<int>& nums)
 {
-    for(auto it = nums.begin(); it != nums.end(); ++it)
-        if((it+1) != nums.end() && *it == *(it+1))
-        {
+    for(auto it = nums.begin(); it != nums.end();)
+        if(it+1 != nums.end() && *it == *(it+1))
             it = nums.erase(it);
-            if((it+1) != nums.end() && *it == *(it+1))
-                it = nums.erase(it);
-        }
+        else
+            ++it;
     return nums.size();
 }
 int main()
 {
-    std::vector<int> nums = {};
+    std::vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
     std::cout << removeDuplicates(nums) << std::endl;
 }
