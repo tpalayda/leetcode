6aa9956184fd4fc86ae3eb5c713d1b48373d177e
tpalayda <taras.palayda@gmail.com>
2018-08-02 19:42:12 +0200

    t14

--- t14.cpp
+++ t14.cpp
@@ -4,9 +4,20 @@
 
 std::string longestCommonPrefix(const std::vector<std::string>& strs)
 {
+    if(strs.empty())
+        return "";
+    std::string result = "";
+    for(unsigned i = 0; i < strs.size(); result += strs[0][i], ++i)
+        for(unsigned j = 0; j < strs.size(); ++j)
+            if(i >= strs[j].size() || (j > 0 && strs[j][i] != strs[j-1][i]))
+                return result;
+    return result;
 }
 
 int main()
 {
-
+    const std::vector<std::string> v1 = {"flower","flow","flight"};
+    const std::vector<std::string> v2 = {"dog","racecar","car"};
+    std::cout << longestCommonPrefix(v1) << std::endl;
+    std::cout << longestCommonPrefix(v2) << std::endl;
 }
