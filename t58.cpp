aec0ac5c4cfe7bf06cc5689f6a25773c88727c97
tpalayda <taras.palayda@gmail.com>
2018-08-05 21:37:51 +0200

    Length of Last Word

--- t58.cpp
+++ t58.cpp
@@ -3,12 +3,18 @@
 size_t lengthOfLastWord(const std::string& s)
 {
     size_t length = 0;
-    for(int j = s.size()-1; j >= 0; ++length,--j)
-        if(s[j] == ' ')
+    for(unsigned i = 0; i < s.size(); ++i)
+    {
+        if(s[i] != ' ')
+            ++length;
+        else if(s[i] == ' ' && i == s.size()-1)
             return length;
+        else if(s[i] == ' ' && i+1 < s.size() && s[i+1] != ' ')
+            length = 0;
+    }
     return length;
 }
 int main()
 {
-    std::cout << lengthOfLastWord(" ") << std::endl;
+    std::cout << lengthOfLastWord("") << std::endl;
 }
