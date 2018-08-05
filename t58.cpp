1f51dfb24e7696783c2a7d9d1c503579ad13b1ec
tpalayda <taras.palayda@gmail.com>
2018-08-05 21:10:06 +0200

    almost

--- t58.cpp
+++ t58.cpp
@@ -2,7 +2,11 @@
 
 size_t lengthOfLastWord(const std::string& s)
 {
-    return 0;
+    size_t length = 0;
+    for(int j = s.size()-1; j >= 0; ++length,--j)
+        if(s[j] == ' ')
+            return length;
+    return length;
 }
 int main()
 {
