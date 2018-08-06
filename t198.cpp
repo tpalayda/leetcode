40f8a0ece18adfd3703073dec2b657f6027392d7
tpalayda <taras.palayda@gmail.com>
2018-08-06 14:37:02 +0200

    House Robber

--- t198.cpp
+++ t198.cpp
@@ -4,16 +4,19 @@
 size_t HouseRobber(const std::vector<int>& nums)
 {
     size_t robEven = 0, robOdd = 0;
+
     for(unsigned i = 0; i < nums.size(); ++i)
         if(i & 1)
-            robOdd += nums[i];
+            robOdd = std::max(robOdd+nums[i],robEven);
         else
-            robEven += nums[i];
-    return robEven > robOdd ? robEven : robOdd;
+            robEven = std::max(robEven+nums[i],robOdd);
+    return std::max(robOdd,robEven);
 }
 
 int main()
 {
     std::vector<int> nums = {2,1,1,2};
+    std::vector<int> nums2 = {1,3,1,3,100};
     std::cout << HouseRobber(nums) << std::endl;
+    std::cout << HouseRobber(nums2) << std::endl;
 }
