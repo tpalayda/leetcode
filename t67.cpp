6b504a89bb77a81402255c83bb8c10faf0a3b634
tpalayda <taras.palayda@gmail.com>
2018-08-07 20:45:47 +0200

    t67 getting started

---
+++ t67.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+std::string addBinary(const std::string& a, const std::string& b)
+{
+    return "";
+}
+
+int main()
+{
+    std::cout << addBinary("11","1") << "\n";
+}
