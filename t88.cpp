10e9b8af210a30826396d1ddc8160860a7050272
tpalayda <taras.palayda@gmail.com>
2018-08-08 09:18:31 +0200

    Merge Sorted Array

--- t88.cpp
+++ t88.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 //Merge Sorted Array
-void merge(std::vector<int& nums1, const int& m, std::vector<int>& nums2, const int& n)
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
+    while(m > 0 && n > 0)
+        if(nums1[m-1] >= nums2[n-1])
+            nums1[m+n-1] = nums1[m---1];
+        else
+            nums1[m+n-1] = nums2[n---1];
+    if(n > 0)
+        for(int i = n-1; i >=0; --i)
+            nums1[i] = nums2[i];
 }
 int main()
 {
-
+    std::vector<int> nums1 = {1,2,3,0,0,0,0};
+    std::vector<int> nums2 = {2,4,6};
+    merge(nums1,3,nums2,3);
+    std::copy(nums1.begin(),nums1.end(),std::ostream_iterator<int>(std::cout," "));
+    std::cout << "\n";
 }
