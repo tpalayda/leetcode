0d9aab9af1da13847e4b6fceebc15f6167ba9ab3
tpalayda <taras.palayda@gmail.com>
2018-08-08 14:47:18 +0200

    Divide Two Integers, idea t29

--- t29.cpp
+++ t29.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
-constexpr int divide(const int& dividend, const int& divisor)
+constexpr int divide(int dividend, int divisor)
 {
-    return 0;
+    int sign = -1;
+    if((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
+        sign = 1;
+    if(dividend < 0)
+        dividend = dividend * -1;
+    if(divisor < 0)
+        divisor = divisor * -1;
+    int result = 0;
+    while(dividend >= divisor)
+    {
+        dividend -= divisor;
+        ++result;
+    }
+    return sign == 1 ? result : -result;
 }
 int main()
 {
-    std::cout << divide(10,3) << std::endl;
+    std::cout << divide(10,10) << std::endl;
 }
