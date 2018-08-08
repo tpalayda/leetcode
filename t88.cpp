89384eea1335b8a5df6a5c7c759b0f7fc64fbb58
tpalayda <taras.palayda@gmail.com>
2018-08-08 09:44:27 +0200

    Merge Sorted Array

--- t88.cpp
+++ t88.cpp
@@ -6,19 +6,15 @@
 void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
     while(m > 0 && n > 0)
-        if(nums1[m-1] >= nums2[n-1])
-            nums1[m+n-1] = nums1[m---1];
-        else
-            nums1[m+n-1] = nums2[n---1];
-    if(n > 0)
-        for(int i = n-1; i >=0; --i)
-            nums1[i] = nums2[i];
+        nums1[m+n-1] = nums1[m-1] >= nums2[n-1] ? nums1[m---1] : nums2[n---1];
+    while(n > 0)
+        nums1[n-1] = nums2[n---1];
 }
 int main()
 {
     std::vector<int> nums1 = {1,2,3,0,0,0,0};
-    std::vector<int> nums2 = {2,4,6};
-    merge(nums1,3,nums2,3);
+    std::vector<int> nums2 = {1,1,1,1};
+    merge(nums1,3,nums2,4);
     std::copy(nums1.begin(),nums1.end(),std::ostream_iterator<int>(std::cout," "));
     std::cout << "\n";
 }
