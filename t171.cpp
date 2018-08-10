3b14dfef21cda86db5129a32033153ec06a869bc
tpalayda <taras.palayda@gmail.com>
2018-08-10 20:36:28 +0200

    Excel Sheet Column Number

--- t171.cpp
+++ t171.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <stirng>
+#include <string>
 
 unsigned titleToNumber(const std::string& s)
 {
-    return 0;
+    unsigned result = 0;
+    for(const char& ch : s)
+        result = result * 26 + (ch - 'A' + 1);
+    return result;
 }
 int main()
 {
-    std::cout << titleToNumber("AA") << std::endl;
+    std::cout << titleToNumber("") << std::endl;
 }
