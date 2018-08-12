f56bec074e287301e1d60ac4504641d126266aa6
tpalayda <taras.palayda@gmail.com>
2018-08-12 18:32:51 +0200

    Uncommon Words from Two Sentences

--- t888.cpp
+++ t888.cpp
@@ -2,10 +2,36 @@
 #include <vector>
 #include <string>
 #include <iterator>
+#include <unordered_map>
 
 std::vector<std::string> uncommonFromSentences(const std::string& A, const std::string& B)
 {
-    return {"sdds","SDDS"};
+    std::unordered_map<std::string,unsigned> hashTable;
+    std::vector<std::string> v;
+    std::string temp;
+    for(const char& ch : A)
+        if(ch == ' ')
+        {
+            ++hashTable[temp];
+            temp = "";
+        }
+        else
+            temp += ch; 
+    ++hashTable[temp]; //last word
+    temp = "";
+    for(const char& ch : B)
+        if(ch == ' ')
+        {
+            ++hashTable[temp];
+            temp = "";
+        }
+        else
+            temp += ch;
+    ++hashTable[temp]; //last word
+    for(const auto& it : hashTable)
+        if(it.second == 1)
+            v.emplace_back(it.first);
+    return v;
 }
 int main()
 {
