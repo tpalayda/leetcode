79bf7e7d054a3908685313de4c95016c5480cd01
tpalayda <taras.palayda@gmail.com>
2018-08-12 18:51:07 +0200

    t819 getting started

---
+++ t819.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string mostCommonWord(const std::string& paragraph, const std::vector<std::string>& banned)
+{
+    return "";
+}
+
+int main()
+{
+
+}
