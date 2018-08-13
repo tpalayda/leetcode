6f666f3d641377843b3df287f004f1b72045ed27
tpalayda <taras.palayda@gmail.com>
2018-08-13 11:30:26 +0200

    Rotate String

--- t796.cpp
+++ t796.cpp
@@ -3,32 +3,7 @@
 
 bool rotateString(const std::string& A, const std::string& B)
 {
-    if(A.empty() && B.empty())
-        return true;
-    if(A.empty() && !B.empty() || !A.empty() && B.empty())
-        return false;
-
-    const char first_character = A[0];
-    auto it = B.begin();
-
-    while(*it++ != first_character)
-        if(it == B.end())
-            return false;
-    it -= 1;
-
-    std::string temp;
-    temp += *it;
-    for(auto it2 = it+1; it2 != it; ++it2)
-    {
-        if(it2 == B.end())
-        {
-            it2 = B.begin();
-            if(it2 == it)
-                break;
-        }
-        temp += *it2;
-    }
-    return temp == A;
+    return A.size() == B.size() && (A+A).find(B) != std::string::npos;
 }
 
 int main()
