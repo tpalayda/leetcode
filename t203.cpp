7fabcadb634eae9285615e10a888abd958173894
tpalayda <taras.palayda@gmail.com>
2018-08-14 19:57:28 +0200

    idea, save

---
+++ t203.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(const int& x) : val(x), next(nullptr) {}
+};
+void insert(ListNode*& root, const int& val)
+{
+    ListNode* node = new ListNode(val);
+    node->next = root;
+    root = node;
+}
+void printListNode(const ListNode* root)
+{
+    while(root)
+    {
+        std::cout << root->val << " ";
+        root = root->next;
+    }
+    std::cout << std::endl;
+}
+ListNode* reverseListNode(ListNode* root)
+{
+    if(!root  || !root->next)
+        return root;
+    ListNode* temp = root->next;
+    root->next = nullptr;
+    while(temp)
+    {
+        ListNode* node = temp->next;
+        temp->next = root;
+        root = temp;
+        temp = node;
+    }
+    return root;
+}
+//remove elements which have value val
+ListNode* removeElements(ListNode* head, const int& val)
+{
+    if(!head)
+        return nullptr;
+    ListNode* root = head->next;
+    ListNode* prev = head;
+    if(prev->val == val)
+    {
+        delete prev;
+        head = head->next;
+    }
+    while(root)
+    {
+        if(root->val == val)
+        {
+            ListNode* temp = root->next;
+            delete root;
+            root = prev;
+            prev->next = temp;
+        }
+        else
+        {
+            prev = root;
+            root = root->next;
+        }
+    }
+    return head;
+}
+
+void DestroyListNode(const ListNode* root)
+{
+    if(root && root->next)
+        DestroyListNode(root->next);
+    delete root;
+}
+int main()
+{
+    ListNode* root = nullptr;
+    for(unsigned i = 5; i > 0; --i)
+        insert(root,i);
+    insert(root,3);
+    insert(root,3);
+    insert(root,3);
+    printListNode(root);
+    printListNode(removeElements(root,3));
+    DestroyListNode(root);
+}
