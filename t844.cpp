a5e4f24b2274c5eb11761034abe0543ddb454ca8
tpalayda <taras.palayda@gmail.com>
2018-08-15 15:45:23 +0200

    t844 getting started

---
+++ t844.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+bool backspaceCompare(const std::string& s, const std::string& t)
+{
+    return 0;
+}
+int main()
+{
+    std::cout << backspaceCompare("ab#c","ad#c") << std::endl;
+}
