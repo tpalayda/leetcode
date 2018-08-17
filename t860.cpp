3bf38cf24724cf45a81d5d7e914eb1378bd7f39a
tpalayda <taras.palayda@gmail.com>
2018-08-17 17:23:39 +0200

    t860 getting started

---
+++ t860.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+bool lemonadeChange(const std::vector<int>& bills)
+{
+    return 0;
+}
+
+int main()
+{
+    std::vector<int> v = {5,5,5,10,20};
+    std::cout << lemonadeChange(v) << std::endl;
+}
