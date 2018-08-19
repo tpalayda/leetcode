2cdf0b230f011251df1da0fb971873900c20f7f0
tpalayda <taras.palayda@gmail.com>
2018-08-19 21:29:32 +0200

    Fair Candy Swap t888

--- t888.cpp
+++ t888.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 //Fair Candy Swap
 std::vector<int> fairCandySwap(const std::vector<int>& A, const std::vector<int>& B)
 {
-    return {1,2};
+    int balans = std::accumulate(B.begin(), B.end(), 0);
+    balans = std::accumulate(A.begin(), A.end(), balans, std::minus<int>());
+    balans /= 2;
+
+    for(const int& el : A)
+        for(const int& el2 : B)
+            if(el2 - el == balans)
+                return {el,el2};
+    return {};
 }
 
 int main()
