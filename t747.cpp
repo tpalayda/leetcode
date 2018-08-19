afadd59fd2062dc2d9715e01e9800466ff72392b
tpalayda <taras.palayda@gmail.com>
2018-08-19 22:02:10 +0200

    t747 getting started

---
+++ t747.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+//Largest Number At Least Twice of Others
+int dominantIndex(const std::vector<int>& nums)
+{
+    return 0;
+}
+
+int main()
+{
+    std::cout << dominantIndex({1,2,3,4}) << '\n';
+}
+
