c2f44c6dc53f98dfc39cd5436a7d91ea8c5171c4
tpalayda <taras.palayda@gmail.com>
2018-08-19 22:11:34 +0200

    Largest Number At Least Twice of Others

--- t747.cpp
+++ t747.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 //Largest Number At Least Twice of Others
 int dominantIndex(const std::vector<int>& nums)
 {
-    return 0;
+    int largest_element = *std::max_element(nums.begin(), nums.end());
+    const size_t n = nums.size();
+
+    for(unsigned i = 0; i < n; ++i)
+        if(nums[i] != largest_element && largest_element < nums[i]*2)
+            return -1;
+    for(unsigned i = 0; i < n; ++i)
+        if(nums[i] == largest_element)
+            return i;
 }
 
 int main()
 {
     std::cout << dominantIndex({1,2,3,4}) << '\n';
+    std::cout << dominantIndex({3,6,1,0}) << '\n';
 }
 
