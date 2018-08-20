e47bb731b6f84be6e781d5ad3ad6216da0eb0dfa
tpalayda <taras.palayda@gmail.com>
2018-08-20 16:27:40 +0200

    Reverse Bits

--- t190.cpp
+++ t190.cpp
@@ -3,7 +3,10 @@
 //Reverse Bits
 uint32_t reverseBits(uint32_t n)
 {
-    return 0;
+    uint32_t result = 0;
+    for(unsigned i = 0; i < 32; ++i)
+        result = (result << 1) + (n >> i & 1);
+    return result;
 }
 int main()
 {
