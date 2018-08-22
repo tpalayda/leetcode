73470cc068e2fa1c2640cedc7431708a9dd2310d
tpalayda <taras.palayda@gmail.com>
2018-08-22 16:07:44 +0200

    t290 idea, save

--- t290.cpp
+++ t290.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <iterator>
+#include <unordered_map>
 
 //Word Pattern
 bool wordPattern(const std::string& pattern, const std::string& str)
 {
+    std::vector<std::string> v;
+    std::string temp;
+    for(const char& ch : str)
+        if(ch == ' ')
+        {
+            v.emplace_back(temp);
+            temp = "";
+        }
+        else
+            temp += ch;
+    v.emplace_back(temp);
+
+    std::unordered_map<char,std::string> hashTable;
+    for(unsigned i = 0; i < pattern.size(); ++i)
+        if(!hashTable.count(pattern[i]))
+            hashTable[pattern[i]] = v[i];
+
+    std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>(std::cout, " "));
     return 0;
 }
 
