7cf5e57bbe52377d4a58a10ff3cec660abdf8c4a
tpalayda <taras.palayda@gmail.com>
2018-08-22 15:58:18 +0200

    t290 Word Pattern getting started

---
+++ t290.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+//Word Pattern
+bool wordPattern(const std::string& pattern, const std::string& str)
+{
+    return 0;
+}
+
+int main()
+{
+    std::cout << wordPattern("abba","dog cat cat dog") << std::endl;
+}
