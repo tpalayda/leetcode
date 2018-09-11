21a0c991c2e217424648a55e64fc28774df6ca72
tpalayda <taras.palayda@gmail.com>
2018-09-11 16:33:35 +0200

    output which depends on number of called function, hmmm

--- t896.cpp
+++ t896.cpp
@@ -4,6 +4,10 @@
 bool isMonotonic(const std::vector<int>& A)
 {
     auto it = A.begin();
+    while(*it == *(it+1)) ++it;
+    if(it == A.end())
+        return true;
+
     if(*it >= *(it+1))
         while(*it >= *(it+1) && it+1 != A.end()) ++it;
     else
@@ -17,9 +21,9 @@ int main()
     std::cout << isMonotonic({6,5,4,4}) << std::endl; //true
     std::cout << isMonotonic({1,3,2}) << std::endl; //false
     std::cout << isMonotonic({1,2,4,5}) << std::endl; //true
-    std::cout << isMonotonic({1,1,1}) << std::endl; //true
     std::cout << isMonotonic({1}) << std::endl; //true
     std::cout << isMonotonic({0,5,1,2,3,575,44,123,12,3,546}) << std::endl; //false
+    std::cout << isMonotonic({1,1,1}) << std::endl; //true
     std::cout << isMonotonic({1,1,2}) << std::endl; //true
 }
 
