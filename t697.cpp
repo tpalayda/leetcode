276db5f7a353fba14282b2553c0f5feb82355660
tpalayda <taras.palayda@gmail.com>
2018-09-22 21:58:41 +0200

    Degree of an Array

--- t697.cpp
+++ t697.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
 
 // 697. Degree of an Array
 // Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
 // Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
 
-unsigned findShortestSubArray(const std::vector<int>& nums)
+int findShortestSubArray(const std::vector<int>& nums)
 {
-    std::unordered_map<int, unsigned> occurrences;
-    std::unordered_set<int> duplicates;
-
-    std::pair<int, unsigned> number;
-    for(const int& num : nums)
-        ++occurrences[num];
-    for(const auto& it : occurrences)
-        if(number.second < it.second)
-            number = it;
-
-    duplicates.emplace(number.first);
-
-    for(const auto& it : occurrences)
-        if(number.second == it.second && number.first != it.first)
-            duplicates.emplace(it.first);
-
-    if(duplicates.size() < 2)
-    {
-        int i = 0, j = nums.size() - 1;
-        while(i > j)
-            if(nums[i] != number.first)
-                ++i;
-            if(nums[j] != number.first)
-                --j;
-            else
-                return i + j;
-    }
-    else
-    {
-        int i = 0, j = nums.size() - 1;
-        while(i > j)
-            if(nums[i] != number.first)
-                ++i;
-            if(nums[j] != number.first)
-                --j;
-            else
-                return i + j;
-    }
-    //std::cout << number.first << ',' << number.second << ',' << isUnique << std::endl;
+    int shortestSize = nums.size();
+    std::unordered_map<int, std::vector<int>> map;
+    for(unsigned i = 0; i < shortestSize; ++i)
+        map[nums[i]].emplace_back(i);
+    int degree = 0;
+    for(const auto& it : map)
+        degree = std::max(degree, static_cast<int>(it.second.size()));
+    for(const auto& it : map)
+        if(it.second.size() == degree)
+            shortestSize = std::min(shortestSize, it.second.back() - it.second[0] + 1);
+    return shortestSize;
 }
 
 int main()
