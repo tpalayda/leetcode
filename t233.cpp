b8c8d7be13943ed98b33bfdc1fb15b58f0310fae
tpalayda <taras.palayda@gmail.com>
2018-09-25 16:09:23 +0200

    Number of Digit One

--- t233.cpp
+++ t233.cpp
@@ -3,27 +3,11 @@
 // 233. Number of Digit One
 // Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
 
-constexpr unsigned doesContainOne(int num)
-{
-    int digit = num % 10;
-    unsigned counter = 0;
-    while(num)
-    {
-        if(digit == 1)
-            ++counter;
-        num /= 10;
-        digit = num % 10;
-    }
-    return counter;
-}
-
 constexpr unsigned countDigitOne(const int& n)
 {
-    if(n <= 0)
-        return 0;
     unsigned counter = 0;
-    for(unsigned i = 1; i <= n; ++i)
-        counter += doesContainOne(i);
+    for(long long i = 1; i <= n; i *= 10)
+        counter += (n/i + 8) / 10 * i + (n/i % 10 == 1) * (n%i + 1);
     return counter;
 }
 
@@ -31,4 +15,5 @@ int main()
 {
     std::cout << countDigitOne(13) << std::endl;
     std::cout << countDigitOne(-1) << std::endl;
+    std::cout << countDigitOne(824883294) << std::endl;
 }
