90f9d5d0af8b4c0311747dc8b057c398909e1db8
tpalayda <taras.palayda@gmail.com>
2018-09-25 13:54:13 +0200

    Self Dividing Numbers

--- t728.cpp
+++ t728.cpp
@@ -2,29 +2,14 @@
 #include <vector>
 #include <iterator>
 
-constexpr bool doesContainZero(int num)
-{
-    int digit = num % 10;
-    while(num /= 10)
-    {
-        if(!digit)
-            return true;
-        digit = num % 10;
-    }
-    return false;
-}
-
 constexpr bool isSelfDividing(const int& num)
 {
-    if(doesContainZero(num))
-        return false;
-
     int temp = num;
     int digit = temp % 10;
 
     while(temp) 
     {
-        if(num % digit)
+        if(!digit || num % digit)
             return false;
         temp /= 10;
         digit = temp % 10;
@@ -43,6 +28,6 @@ inline std::vector<int> selfDividingNumbers(const int& left, const int& right)
 
 int main()
 {
-    const std::vector<int> result = selfDividingNumber(1, 22);
+    const std::vector<int> result = selfDividingNumbers(1, 22);
     std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
 }
