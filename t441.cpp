58888eda3375d1c2c73b3d21efb8dc07448b87c4
tpalayda <taras.palayda@gmail.com>
2018-09-27 21:12:56 +0200

    Arranging Coints upd

--- t441.cpp
+++ t441.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
+#include <cmath>
 
 constexpr unsigned arrangeCoins(const unsigned& n)
 {
-    unsigned sum = 0;
-    unsigned counter = 0;
-    for(unsigned k = 1;; ++k)
-    {
-        sum += k;
-        ++counter;
-        if(sum > n)
-            return counter - 1;
-    }
-    return sum;
+    return sqrt(2 * static_cast<double>(n) + 0.25) - 0.5;
 }
 
 int main()
 {
     std::cout << arrangeCoins(5) << std::endl; //2
     std::cout << arrangeCoins(8) << std::endl; //3
-    std::cout << arrangeCoins(1) << std::endl; //3
+    std::cout << arrangeCoins(1) << std::endl; //1
 }
