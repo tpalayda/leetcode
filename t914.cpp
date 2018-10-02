184a4bd00e36ec0ac05070523928c110aa49a14e
tpalayda <taras.palayda@gmail.com>
2018-10-02 12:58:24 +0200

    55/69 test cases passed, idea v1

--- t914.cpp
+++ t914.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 bool hasGroupsSizeX(const std::vector<int>& deck) 
 {
-    return false;
+    std::unordered_map<int, unsigned> occurrences;
+    const size_t deckSize = deck.size();
+    if(deckSize & 1)
+        return false;
+    for(const int& num : deck)
+        ++occurrences[num];
+    for(const auto& it : occurrences)
+        if(it.second & 1)
+            return false;
+    return true;
 }
 
 int main()
@@ -13,4 +23,6 @@ int main()
     std::cout << hasGroupsSizeX({1}) << std::endl; //0
     std::cout << hasGroupsSizeX({1, 1}) << std::endl; //1
     std::cout << hasGroupsSizeX({1, 1, 2, 2, 2, 2}) << std::endl; //1
+    std::cout << hasGroupsSizeX({1, 1, 2, 2, 2, 2}) << std::endl; //1
+    std::cout << hasGroupsSizeX({0, 0, 0, 1, 1, 1, 2, 2, 2}) << std::endl; //1
 }
