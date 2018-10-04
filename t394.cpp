b576f2759ba109e42f3f47e6a8ba3e79e1ed0ff2
tpalayda <taras.palayda@gmail.com>
2018-10-04 16:34:40 +0200

    hmmm

--- t394.cpp
+++ t394.cpp
@@ -2,15 +2,42 @@
 #include <string>
 
 // 394. Decode String
+constexpr bool isInteger(const char& c)
+{
+    return c > '0' && c <= '9';
+}
 
 std::string decodeString(const std::string& s)
 {
-    return "";
+    int multiplier = 0;
+    std::string temp;
+    std::string result;
+    for(auto it = s.begin(); it != s.end();)
+    {
+        if(isInteger(*it))
+        {
+            multiplier = *it - 48;
+            it += 2;
+        }
+        else if(*it != '[' && *it != ']')
+            temp += *it++;
+        else
+        {
+            std::cout << "end of bracket:" << temp << ',' << multiplier << std::endl;    
+
+            for(unsigned i = 0; i < multiplier; ++i)
+                result = result + temp;            
+            temp = "";
+            it++;
+        }
+        
+    }
+    return result;
 }
 
 int main()
 {
-    std::cout << decodeString("3[a]2[bc]") << std::endl;
-    std::cout << decodeString("3[a2[c]]") << std::endl;
-    std::cout << decodeString("2[abc]3[cd]ef") << std::endl;
+    std::cout << decodeString("3[a]2[bc]") << std::endl; //aaabcbc
+    std::cout << decodeString("3[a2[c]]") << std::endl; //accaccacc
+    std::cout << decodeString("2[abc]3[cd]ef") << std::endl; //abcabccdcdcdef
 }
