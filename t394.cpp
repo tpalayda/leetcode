3bdbd27eed6452b3feb1009b6ba320306e8a0daf
tpalayda <taras.palayda@gmail.com>
2018-10-04 13:18:05 +0200

    TODO today

---
+++ t394.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+// 394. Decode String
+
+std::string decodeString(const std::string& s)
+{
+    return "";
+}
+
+int main()
+{
+    std::cout << decodeString("3[a]2[bc]") << std::endl;
+    std::cout << decodeString("3[a2[c]]") << std::endl;
+    std::cout << decodeString("2[abc]3[cd]ef") << std::endl;
+}
