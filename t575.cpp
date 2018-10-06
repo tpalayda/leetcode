add63b6699fa40a2edbd450d502c6c40a93d4201
tpalayda <taras.palayda@gmail.com>
2018-10-06 11:33:32 +0200

    Distribute Candies

--- t575.cpp
+++ t575.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 
 // 575. Distribute Candies
 unsigned distributeCandies(const std::vector<int>& candies)
 {
-    const size_t numberOfCandies = candies.size();
-    unsigned result = 0;
-    unsigned givenToBrother = 0;
-
-    std::unordered_map<int, unsigned> occurrences;
-    for(const int& candie : candies)
-        ++occurrences[candie];
-
-    unsigned numberOfUniqueCandies = occurrences.size();
-    if(numberOfUniqueCandies <= numberOfCandies/2)
-        return numberOfUniqueCandies;
-    else
-    {
-        for(const auto& it : occurrences)
-            if(it.second >= 1 && result < numberOfCandies/2)
-                ++result;
-    }
-    return result;
+    return std::min(std::unordered_set<int>(candies.begin(), candies.end()).size(), candies.size()/2);
 }
 
 int main()
